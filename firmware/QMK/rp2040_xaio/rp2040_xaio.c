8bc0f8484600211bfc152b5fcd77579fdd19c274
techy-robot <asher.j.edwards1@gmail.com>
2025-08-22 16:00:32 -0600

    Removed unused code

--- firmware/QMK/rp2040_xaio/rp2040_xaio.c
+++ firmware/QMK/rp2040_xaio/rp2040_xaio.c
@@ -51,18 +51,6 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-//Encoder main purpose is to switch key layers and input modes (game controller, macropad, mouse, remote)
-
-//This should be encoder matrix, which doesn't exist in QMK and I will have to write my own.
-/*#ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
-};*/
-
-
-// All below needs to be moved into a file called joykey.c, as its custom utility functions and not usr
-
-
 //joystick
 //if in mouse mode return mouse movements, otherwise return joystick values
 // Need to write that code
@@ -72,30 +60,3 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };*/
 
 //Gyro sensor data, used for mouse, or game controller only
-
-
-//wireless stuff??
-
-/*#ifdef OLED_ENABLE
-bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-
-    switch (get_highest_layer(layer_state)) {
-        case 0:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
-    
-    return false;
-}
-#endif*/
