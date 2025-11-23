ffd53d26d534e645df7a1232f2a2949f1a006fda
techy-robot <asher.j.edwards1@gmail.com>
2025-11-23 12:32:31 -0700

    Created settings utilities

---
+++ firmware/QMK/rp2040_xaio/utils/settings.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "raw_hid.h"
+#include "keyname_map.h"
+#include "eeprom.h"
+
+
+// Define a safe offset. Standard QMK usage rarely exceeds 1KB. 
+// 2KB offset is usually safe on large flash chips.
+#define CUSTOM_MAP_EEPROM_OFFSET 2048 
+
+void custom_eeprom_save(void) {
+    // Write the entire raw struct to EEPROM
+    // NOTE: eeprom_update_block is slow. Only call this when necessary.
+    eeprom_update_block(keyname_map, (void*)CUSTOM_MAP_EEPROM_OFFSET, sizeof(keyname_map));
+}
+
+void custom_eeprom_load(void) {
+    eeprom_read_block(keyname_map, (const void*)CUSTOM_MAP_EEPROM_OFFSET, sizeof(keyname_map));
+}
